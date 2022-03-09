988feab4c86ea6b8235da79dd56f5f4af70b5b1b
Max Kakurin <iveilwallet@yandex.ru>
2022-03-09 23:14:02 +0300

    v7.1 com Push

---
+++ src/models/UserStudents.ts
@@ -0,0 +1,11 @@
+export interface Student {
+  id: number;
+  email: string;
+  login: string;
+  password: string;
+  name: string;
+  surname: string;
+  patronymic: string;
+  dateBirth: string;
+  studyGroup: string;
+}
